488e551692ef14540147f5a47bef0dcbe78be183
marcomoreschi <marcomoreschi91@hotmail.it>
2020-05-22 09:32:08 +0000

    Added footer

--- app.py
+++ app.py
@@ -1,7 +1,13 @@
 import os
-from flask import Flask
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 
 app = Flask(__name__)
+app.config["MONGO_DBNAME"] ='marco_cook'
+app.config["MONGO_URI"] = 'mongodb+srv://root:r00tUser@myfirstcluster-z4c90.mongodb.net/marco_cook?retryWrites=true&w=majority'
+
+mongo =PyMongo(app)
 
 @app.route('/')
 def hello():
