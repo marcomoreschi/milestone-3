b33103059891af30bbf5081ba8c1a82059aabcc9
marcomoreschi <marcomoreschi91@hotmail.it>
2020-05-27 14:36:29 +0000

    added addrecipe.html

--- app.py
+++ app.py
@@ -9,25 +9,39 @@ app.config["MONGO_URI"] = 'mongodb+srv://root:r00tUser@myfirstcluster-z4c90.mong
 
 mongo =PyMongo(app)
 
-#-------------------#Home Page
-
+#////////////////////////////////////////////////# Add Home Page
 @app.route('/')
 @app.route('/index')
 def index():
     recipes=mongo.db.recipes.find()
     return render_template("index.html", recipes=recipes)
 
+
+#////////////////////////////////////////////////# Add saltimbocca 
 @app.route('/saltimbocca')
 def saltimbocca():
     return render_template("saltimbocca.html")
 
+
+#////////////////////////////////////////////////# Add pasta alla norma
 @app.route('/pastaallanorma')
 def pastaallanorma():
     return render_template("pastaallanorma.html")
 
+
+#////////////////////////////////////////////////# Add fish and chips
 @app.route('/fishandchips')
 def fishandchips():
     return render_template("fishandchips.html")
+
+
+#////////////////////////////////////////////////# Add recipe page
+@app.route('/add_recipe')
+def add_recipe():
+    return render_template("addrecipe.html",
+    cuisine=mongo.db.cuisine_recipe.type_recipe.find())
+
+
 
 
 
