cd924ceba6c7729e5ad4d0749630bfbd72e1f765
marcomoreschi <marcomoreschi91@hotmail.it>
2020-06-04 14:40:13 +0000

    Fixed import env file in app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import os
-import env
+if os.path.exists('env.py'):
+    import env 
 from flask import Flask, render_template, redirect, request, url_for
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
